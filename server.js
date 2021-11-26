bbbdb5cdc72d6de6a9ef9f47e9f9d05714f9ca73
Fabio <fabioaoad@gmail.com>
2021-11-26 13:22:45 -0300

    Servir contenido estatico en la carpeta public - S07A65

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    //res.send('Hola Mundo');
-    let salida = {
-        nombre: 'Fabio Martin',
-        apellido: 'Aoad',
-        url: req.url
-    };
-    res.send(salida);
-})
+//midleware
+app.use( express.static( __dirname + '/public' ) );
+
+
+
+
+// app.get('/', (req, res) => {
+//     //res.send('Hola Mundo');
+//     let salida = {
+//         nombre: 'Fabio Martin',
+//         apellido: 'Aoad',
+//         url: req.url
+//     };
+//     res.send(salida);
+// });
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
