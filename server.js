d028d7b90ec44ec9bd181afd953bd4593561fa25
Fabio <fabioaoad@gmail.com>
2021-11-28 19:21:21 -0300

    Usando parciales con HBS - S07A68

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 //midleware
 app.use( express.static( __dirname + '/public' ) );
 
-// express hbs
+// express hbs y directorio de los parciales
+hbs.registerPartials( __dirname + '/views/parciales' );
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
@@ -14,6 +16,12 @@ app.get('/', (req, res) => {
    });
 });
 
+
+app.get('/about', (req, res) => {
+    res.render('about',{
+        anio: new Date().getFullYear()
+    });
+});
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
 });
