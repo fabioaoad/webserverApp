7719f350a5f0ad52fd09119aa36507c637a1b878
Fabio <fabioaoad@gmail.com>
2021-11-28 18:34:48 -0300

    Handlebars - S07A67

--- server.js
+++ server.js
@@ -4,18 +4,15 @@ const app = express();
 //midleware
 app.use( express.static( __dirname + '/public' ) );
 
+// express hbs
+app.set('view engine', 'hbs');
 
-
-
-// app.get('/', (req, res) => {
-//     //res.send('Hola Mundo');
-//     let salida = {
-//         nombre: 'Fabio Martin',
-//         apellido: 'Aoad',
-//         url: req.url
-//     };
-//     res.send(salida);
-// });
+app.get('/', (req, res) => {
+   res.render('home',{
+       nombre: 'Fabio Martin Aoad',
+       anio: new Date().getFullYear()
+   });
+});
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
