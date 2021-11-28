88a9e1ef72a6752e34aace46aaf9c0f7ddb6c19d
Fabio <fabioaoad@gmail.com>
2021-11-28 19:39:39 -0300

    Revert "Usando helpers de HBS - S07A68"
    This reverts commit 5a939603d66efa296817d8e21dcf83eba4c9c324.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-require('./hbs/helpers');
-
 
 //midleware
 app.use( express.static( __dirname + '/public' ) );
@@ -11,18 +9,18 @@ app.use( express.static( __dirname + '/public' ) );
 hbs.registerPartials( __dirname + '/views/parciales' );
 app.set('view engine', 'hbs');
 
-
-
-
 app.get('/', (req, res) => {
    res.render('home',{
-       nombre: 'Fabio MarTin AoAd'
+       nombre: 'Fabio Martin Aoad',
+       anio: new Date().getFullYear()
    });
 });
 
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about',{
+        anio: new Date().getFullYear()
+    });
 });
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
